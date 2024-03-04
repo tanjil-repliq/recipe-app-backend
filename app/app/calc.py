2be50fb3f010b2ac9352834f40cdc12d28b8ba68
Md Tanjil <125866000+tanjil-repliq@users.noreply.github.com>
2024-03-04 16:52:37 +0600

    Added multiply and divide with test cases

--- app/app/calc.py
+++ app/app/calc.py
@@ -11,3 +11,13 @@ def add(x, y):
 def subtract(x, y):
     """Subtract x and y and return the result"""
     return y - x
+
+
+def multiply(x, y):
+    """Multiply x and y and return the result"""
+    return x * y
+
+
+def divide(x, y):
+    """Divide x and y and return the result"""
+    return x / y
--- app/app/tests.py
+++ app/app/tests.py
@@ -20,3 +20,12 @@ class CalcTests(SimpleTestCase):
         """Test subtracting numbers"""
         res = calc.subtract(10, 15)
         self.assertEqual(res, 5)
+
+    def test_multiply_numbers(self):
+        """Test multiply numbers"""
+        res = calc.multiply(5, 2)
+        self.assertEqual(res, 10)
+
+    def test_divide_numbers(self):
+        res = calc.divide(6, 2)
+        self.assertEqual(res, 3)
